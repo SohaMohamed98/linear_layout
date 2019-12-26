24a4cbb72de31ec7e0d52459c0dede8f2eb8135e
sohamohamed <sohamohamed98@github.com>
2019-12-26 14:35:44 +0200

    linear

---
+++ app/src/main/java/com/example/linearlayout/GravityLayout.kt
@@ -0,0 +1,12 @@
+package com.example.linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GravityLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gravity_layout)
+    }
+}
---
+++ app/src/main/java/com/example/linearlayout/Horizontal.kt
@@ -0,0 +1,12 @@
+package com.example.linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Horizontal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_horizontal)
+    }
+}
